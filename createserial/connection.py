2757efa37498736ec9b08697d9622e213cef9757
lhub555 <99777109+lhub555@users.noreply.github.com>
2022-05-09 20:54:51 -0400

    Create connection.py

---
+++ createserial/connection.py
@@ -0,0 +1,14 @@
+from createserial.serial import open_serial, close_serial
+from createserial.commands import open_create, close_create
+
+
+def connect():
+    """Connect to the create in a safe manner"""
+    open_serial()  # open a serial port connection to the Create
+    reset_create()  # reset Create to start with a clean slate
+    open_create()  # initialize the Create
+    
+def disconnect():
+    """Disconnect from the create in a safe manner"""
+    close_create()
+    close_serial()
