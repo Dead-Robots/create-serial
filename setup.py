af660c81796133ef4016b35af49be8354d68217d
graysonelias <graysonelias@gmail.com>
2023-03-11 15:04:52 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='createserial',
+    version='1.0',
+    packages=['createserial'],
+    url='https://github.com/Dead-Robots/create-serial',
+    license='',
+    author='DRS',
+    author_email='',
+    description='Python serial interface for Create'
+)
