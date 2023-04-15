74b42efcd5852f63a800fef621268e917cbc839d
Grayson Elias <graysonelias@gmail.com>
2023-04-14 22:30:11 -0400

    Fix the stop button
    Added a Watcher class that is created in the CreateConnection context manager. This represents a second process that spawns when the context manager is entered. This process waits for the parent process to be killed without cleaning up. When the parent is killed it connects to the Create and shuts it off and disconnects.
    
    Closes #9

--- createserial/__init__.py
+++ createserial/__init__.py
@@ -1,18 +1,18 @@
 import atexit
 import signal
-from createserial.commands import close_create
-from createserial.serial import close_serial, ser
+from createserial.connection import disconnect
+
 
 def handle_exit():
     print("Exiting")
-    if ser.is_open:
-        close_create()
-        close_serial()
+    disconnect()
+
 
 def handle_exit_dirty(*args):
     print("Exiting,", args)
     handle_exit()
-    exit(23)
+    exit(42)
+
 
 atexit.register(handle_exit)
 signal.signal(signal.SIGTERM, handle_exit_dirty)
--- createserial/connection.py
+++ createserial/connection.py
@@ -1,5 +1,6 @@
-from createserial.serial import open_serial, close_serial
+from createserial.serial import open_serial, close_serial, ser
 from createserial.commands import open_create, close_create, reset_create
+from createserial.watcher import Watcher
 
 
 def connect():
@@ -7,21 +8,28 @@ def connect():
     open_serial()  # open a serial port connection to the Create
     reset_create()  # reset Create to start with a clean slate
     open_create()  # initialize the Create
-    
+
 
 def disconnect():
     """Disconnect from the create in a safe manner"""
-    close_create()
-    close_serial()
+    if ser.is_open:
+        close_create()
+        close_serial()
+
 
 class CreateConnection:
     """Allow for use the with statement for Create connections"""
-    def __init__(self):
-        pass
+
+    def __init__(self, watcher=True):
+        self._watcher = Watcher() if watcher else None
 
     def __enter__(self):
         connect()
+        if self._watcher:
+            self._watcher.start()
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         disconnect()
+        if self._watcher:
+            self._watcher.signal()
