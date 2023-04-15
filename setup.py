7a7ab3ea09c7076ac851b58275e978df1601dfe2
Grayson Elias <graysonelias@users.noreply.github.com>
2023-04-15 13:07:36 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='createserial',
-    version='1.1',
+    version='1.2',
     packages=['createserial'],
     url='https://github.com/Dead-Robots/create-serial',
     license='',
